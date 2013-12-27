de3ee346734f4cea61721c82fee767401bb7e374
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2013-12-27 15:33:25 +0100

    fixed registration mail

--- app/mails/tools.py
+++ app/mails/tools.py
@@ -159,7 +159,7 @@ def send_error_mail(subject, sender):
     # Sends an error mail to not registred users
     from mails.models import AddressLog
 
-    if AddressLog.objects.all().filter(address=sender) == []:
+    if not AddressLog.objects.filter(address=sender).exists():
         smtp = smtp_login()
         host = settings.EMAIL_ADDRESS.split('@')[1]
         content = get_template('mails/not_registred_mail.txt')
@@ -181,6 +181,8 @@ def send_error_mail(subject, sender):
 
         entry = AddressLog(address=sender)
         entry.save()
+
+        print('Sent registration mail to %s') % sender
 
 
 def send_activation_mail(key, address, host):
