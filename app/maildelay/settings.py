abea462e564867d4304bb7418c17ddc9c7aeed44
Jonas Lehmann <jonas.lehmann@adfinis-sygroup.ch>
2018-06-27 17:19:33 +0200

    Fix template structure to overwrite the admin templates

--- app/maildelay/settings.py
+++ app/maildelay/settings.py
@@ -55,6 +55,9 @@ ROOT_URLCONF = 'maildelay.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+          os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
