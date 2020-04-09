327f1684097228407829a6c59099e0391c508396
Wiktork <wiktor.kepczynski@adfinis-sygroup.ch>
2020-04-09 12:13:53 +0200

    fix missing r in regual expressions

--- app/mails/tools.py
+++ app/mails/tools.py
@@ -24,7 +24,7 @@ def get_delay_days_from_email_address(email_address):
     :rtype: integer
     """
     try:
-        match = re.findall("^(\d+)([dmw])", email_address)[0]
+        match = re.findall(r"^(\d+)([dmw])", email_address)[0]
         multiplicator = settings.EMAIL_SUFFIX_TO_DAY[match[1]]
         delay = int(match[0]) * int(multiplicator)
         return delay
@@ -41,7 +41,7 @@ def get_delay_addresses_from_recipients(recipients):
     """
     delay_addresses = []
     for recipient in recipients:
-        if re.search("^(\d+[dmw])", recipient["email"]):
+        if re.search(r"^(\d+[dmw])", recipient["email"]):
             delay_addresses.append(recipient["email"])
     if delay_addresses:
         return delay_addresses
@@ -57,7 +57,7 @@ def get_key_from_email_address(email_address):
     :rtype: string
     """
     try:
-        return re.search("^\d+[dmw]\.([0-9a-z]{10})@", email_address).group(1)
+        return re.search(r"^\d+[dmw]\.([0-9a-z]{10})@", email_address).group(1)
     except AttributeError:
         return None
 
