b5ba4da9994441840934a783f5016f899a22538e
Wiktork <wiktor.kepczynski@adfinis-sygroup.ch>
2020-04-22 16:46:07 +0200

    simplify sendmail command

--- app/mails/management/commands/sendmail.py
+++ app/mails/management/commands/sendmail.py
@@ -1,4 +1,3 @@
-import smtplib
 import logging
 from django.utils import timezone
 from email.mime.text import MIMEText
@@ -16,13 +15,6 @@ logger = logging.getLogger("mails")
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        try:
-            smtp = smtplib.SMTP(settings.EMAIL_HOST)
-            smtp.starttls()
-            smtp.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
-        except:
-            return
-
         dues = Due.objects.filter(due__lte=timezone.now())
 
         for due in dues:
@@ -39,61 +31,19 @@ class Command(BaseCommand):
 
                 return
 
-            charset = message.msg.get_content_charset()
             recipients = mail.recipient_set
             tpl = get_template("mails/messages/mail_attachment.txt")
             text = tpl.render({"recipients": recipients})
+            msg = None
 
-            if message.msg.is_multipart():
-                add_text = MIMEText(text, "plain", "utf-8")
-                if message.msg.get_content_maintype == "multipart/signed":
-                    # If it's a signed message, only take first payload
-                    msg = MIMEMultipart()
-                    orig = message.msg.get_payload(0)
-                    msg.attach(orig)
-                    msg.attach(add_text)
-                else:
-                    msg = MIMEMultipart()
-                    msg.attach(message.msg)
-                    msg.attach(add_text)
-
-            else:
-                msg = MIMEText(
-                    "\n\n".join((message.msg.get_payload(), str(text))),
-                    "plain",
-                    charset,
-                )
+            send_email_with_attachments(
+                message=message, mail=mail, text=text, msg=msg
+            )
 
             try:
-                for i in message.msg.walk():
-                    if i.get_content_maintype() == "text":
-                        content = i.get_payload(decode=True)
-                        break
-
-                attachments = []
-                for part in msg.walk():
-                    if part.get_content_maintype() == "multipart":
-                        continue
-                    if part.get("Content-Disposition") is None:
-                        continue
-                    attachments.append(part)
-
-                email = EmailMessage(
-                    "Reminder from {}: {}".format(
-                        mail.sent.strftime("%b %d %H:%M"), mail.subject
-                    ),
-                    content.decode("utf-8") + text,
-                    settings.EMAIL_HOST_USER,
-                    [mail.user.email],
+                send_email_with_attachments(
+                    message=message, mail=mail, text=text, msg=msg
                 )
-                for attachment in attachments:
-                    email.attach(
-                        attachment.get_filename(),
-                        attachment.get_payload(decode=True),
-                        attachment.get_content_type(),
-                    )
-                email.send()
-
             except:
                 message.delete()
                 print("Failed to write new header")
@@ -108,4 +58,53 @@ class Command(BaseCommand):
                 message.delete()
                 mail.delete()
 
-        smtp.quit()
+
+def attach_MIMEText_to_mulitpart_messages(message, mail, text, msg):
+    charset = message.msg.get_content_charset()
+
+    if message.msg.is_multipart():
+        add_text = MIMEText(text, "plain", "utf-8")
+        if message.msg.get_content_maintype == "multipart/signed":
+            # If it's a signed message, only take first payload
+            msg = MIMEMultipart()
+            orig = message.msg.get_payload(0)
+            msg.attach(orig)
+            msg.attach(add_text)
+        else:
+            msg = MIMEMultipart()
+            msg.attach(message.msg)
+            msg.attach(add_text)
+
+    else:
+        msg = MIMEText(
+            "\n\n".join((message.msg.get_payload(), str(text))), "plain", charset,
+        )
+
+
+def send_email_with_attachments(message, mail, text, msg):
+    for i in message.msg.walk():
+        if i.get_content_maintype() == "text":
+            content = i.get_payload(decode=True)
+            break
+
+    attachments = []
+    for part in msg.walk():
+        if part.get_content_maintype() == "multipart":
+            continue
+        if part.get("Content-Disposition") is None:
+            continue
+        attachments.append(part)
+
+    email = EmailMessage(
+        "Reminder from {}: {}".format(mail.sent.strftime("%b %d %H:%M"), mail.subject),
+        content.decode("utf-8") + text,
+        settings.EMAIL_HOST_USER,
+        [mail.user.email],
+    )
+    for attachment in attachments:
+        email.attach(
+            attachment.get_filename(),
+            attachment.get_payload(decode=True),
+            attachment.get_content_type(),
+        )
+    email.send()
