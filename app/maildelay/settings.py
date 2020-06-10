f32eabfb493b894bc924416094bb28b6c5f3f116
Wiktork <wiktor.kepczynski@adfinis.com>
2020-06-10 08:29:37 +0200

    sentry integration

--- app/maildelay/settings.py
+++ app/maildelay/settings.py
@@ -13,6 +13,17 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import datetime
 import os
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="",
+    integrations=[DjangoIntegration()],
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
